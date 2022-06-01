8b9cf81162feee315a7ee2cb38ff9be39e74028b
GeorgiPavlov89 <georgi.pavlov89@gmail.com>
2022-06-01 10:27:29 +0300

    Dark Theme
    Finished dark theme switch

--- next-countries/components/Header.js
+++ next-countries/components/Header.js
@@ -1,18 +1,21 @@
 import styles from '../styles/Header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoon } from "@fortawesome/free-regular-svg-icons"
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons"
 
-export default function Header() {
+
+
+export default function Header({ theme, switchTheme }) {
+
     return (
         <div className={styles.title}>
             <h3 className={styles.header}>
                 Where in the world?</h3>
             <div >
-                <FontAwesomeIcon icon={faMoon} />
-                <button className={styles.themeButton} >
-                    Dark Mode
+                <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} />
+                <button className={styles.themeButton} onClick={switchTheme}>
+                    {theme === 'light' ? 'Dark' : 'Light'} Mode
                 </button>
             </div>
-        </div>
+        </div >
     )
 }
--- next-countries/components/SearchAndDropDown.js
+++ next-countries/components/SearchAndDropDown.js
@@ -1,21 +1,45 @@
 import React from 'react'
 import styles from '../styles/SearchAndDD.module.css'
-import { useState } from "react"
+import { useState, useEffect, useRef } from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons"
 
-export default function SearchAndDropDown({ title, regions, multiSelect = false }) {
+export default function SearchAndDropDown({ title, regions, multiSelect = false, handleSearch, searchRef, setSelectRegion }) {
     const [open, setOpen] = useState(false);
-    const [selection, setSelection] = useState([]);
-    const toggle = () => setOpen(!open)
 
-    function handleOnClick(region) { }
+    const toggle = () => setOpen(!open)
+    const dropdown = useRef(null)
+
+    function handleSelectRegion(region) {
+        setSelectRegion(region.value)
+
+    }
+
+    useEffect(() => {
+        if (!open) return;
+        function handleCloseDropdown(e) {
+            if (dropdown.current && !dropdown.current.contains(e.target)) {
+                setOpen(false)
+            }
+        }
+        window.addEventListener("click", handleCloseDropdown);
+        return () => window.removeEventListener("click", handleCloseDropdown);
+
+    }, [open])
+
+
     return (
         <div className={styles.container}>
-            <form className={styles.search}>
+            <div className={styles.search} >
                 <FontAwesomeIcon icon={faSearch} style={{ color: "#9ca3af", marginLeft: "1.5rem" }} />
-                <input name="query" type="search" placeholder="Search for a country..." />
-            </form>
+                <input
+
+                    ref={searchRef}
+                    onKeyDown={handleSearch}
+                    name="query"
+                    type="search"
+                    placeholder="Search for a country..." />
+            </div>
             <div className={styles.ddWraper}>
                 <div
                     className={styles.ddHeader}
@@ -23,9 +47,10 @@ export default function SearchAndDropDown({ title, regions, multiSelect = false 
                     role="button"
                     onKeyPress={() => toggle(!open)}
                     onClick={() => toggle(!open)}
+                    ref={dropdown}
                 >
                     <div className={styles.ddHeader__title}>
-                        <p>{title}</p>
+                        <p>{title.length > 0 ? title : "Filter by Region"}</p>
                     </div>
                     <FontAwesomeIcon icon={open ? faChevronUp : faChevronDown} />
                 </div>
@@ -35,7 +60,7 @@ export default function SearchAndDropDown({ title, regions, multiSelect = false 
                             {
                                 regions.map(region => (
                                     <li key={region.id} >
-                                        <button className={styles.listButton} type="button" onClick={() => handleOnClick(region)}>
+                                        <button className={styles.listButton} type="button" onClick={() => handleSelectRegion(region)}>
                                             <span>{region.value}</span>
                                         </button>
                                     </li>
