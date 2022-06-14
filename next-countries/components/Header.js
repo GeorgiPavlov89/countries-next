0febe1d3738380d0f472d56653f10c45804b3e42
GeorgiPavlov89 <georgi.pavlov89@gmail.com>
2022-06-14 13:00:52 +0300

    Final Touches
    setup Context API and make it responsive

--- next-countries/components/Header.js
+++ next-countries/components/Header.js
@@ -1,19 +1,29 @@
 import styles from '../styles/Header.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons"
+import { CountriesContext } from '../store'
+import { useContext } from 'react';
 
 
+export default function Header() {
+    const ctx = useContext(CountriesContext)
 
-export default function Header({ theme, switchTheme }) {
-
+    const switchTheme = () => {
+        console.log(ctx.theme);
+        if (ctx.theme === 'light') {
+            ctx.setTheme('dark');
+            return
+        }
+        ctx.setTheme('light');
+      }
     return (
-        <div className={styles.title}>
+        <div className={styles.title} data-theme={ctx.theme}>
             <h3 className={styles.header}>
                 Where in the world?</h3>
             <div >
-                <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} />
-                <button className={styles.themeButton} onClick={switchTheme}>
-                    {theme === 'light' ? 'Dark' : 'Light'} Mode
+                <FontAwesomeIcon icon={ctx.theme === "light" ? faMoon : faSun} style={{color: 'var(--text)'}} />
+                <button className={styles.themeButton} onClick={switchTheme} >
+                    {ctx.theme === 'light' ? 'Dark' : 'Light'} Mode
                 </button>
             </div>
         </div >
--- next-countries/pages/country/[country].js
+++ next-countries/pages/country/[country].js
@@ -1,102 +1,43 @@
 import React from 'react'
+import dynamic from "next/dynamic"
+import DetailPage from '../../components/DetailPage'
+import { useState, useEffect, useContext } from 'react'
+import { CountriesContext } from '../../store';
+import { useRouter } from 'next/router'
 import Header from '../../components/Header'
-import styles from '../../styles/country.module.css'
-import Link from 'next/link'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faArrowLeftLong} from '@fortawesome/free-solid-svg-icons'
-import Image from 'next/image'
 
 
-export async function getStaticProps({ params }) {
-    const results = await fetch(`https://restcountries.com/v3.1/name/${params.country}`).then(res => res.json());
-    return {
-        props: {
-            country: results[0]
+
+
+
+
+
+
+export default function country() {
+
+    const [ currentCountry, setCurentCountry ] = useState();
+    const ctx = useContext(CountriesContext)
+    
+    const router = useRouter()
+    useEffect(() => {
+        if (ctx.countries.length === 0) {
+            router.push('/')
         }
-    }
-}
-
-export async function getStaticPaths() {
-    const countries = await fetch('https://restcountries.com/v3.1/all?field=name').then(res => res.json());
-
-    return {
-        paths: countries.map(country => {
-            return {
-                params: {
-                    country: country.name.common.toString()
-                }
-            }
-        }),
-        fallback: false
-    }
-}
-
-export default function country({ country, theme }) {
-    console.log(theme)
-    const currencyArray = Object.values(country.currencies)
-    console.log(currencyArray);
-    const languegesArray = Object.values(country.languages)
-    console.log(languegesArray);
-    return (
-
-        <>
-            <Header />
-            <main className={styles.container} data-theme={theme}>
-                <Link href="/" >
-                <div >
-                
-                    <button className={styles.btn}>
-                    <FontAwesomeIcon icon={faArrowLeftLong} style={{  marginRight: ".5rem", color:"var(--text)" }}/>
-                        Back
-                    </button>
-                    </div>
-                </Link>
-                <div className={styles.image}>
-                <Image src={country.flags.svg} width="300px" height="200px" layout="responsive" objectFit="cover" alt="country flag"  />
-                </div>
-                <div className={styles.info}>
-                    <h3>{country.name.common}</h3>
-                    <p><span>Native Name:</span>{country.name.official}</p>
-                    <p><span>Population:</span>{country.population}</p>
-                    <p><span>Region:</span>{country.region}</p>
-                    <p><span>Region:</span>{country.subregion}</p>
-                    <p><span>Region:</span>{country.capital}</p>
-                </div>
-                <div className={styles.info}>
-                <p><span>Top Level Domain:</span>{country.tld}</p>
-               {
-                currencyArray.map(({ name }) => {
-        return <p key={name}><span>Currencies:</span>{name}</p>
+        setCurentCountry(
+            ctx.countries.find((country) => country.cca3 === router.query.country)
+        )
     })
-               }
-
-               {
-                   languegesArray.map((index) => {
-                       return <p key={index}><span>Languages:</span>{languegesArray}</p>
-                   })
-               }
-                </div>
-                <div className={styles.borders}>
-                <span>Border Countries:</span>
-                <div className={styles.borderBtns}>
-                {
-                    
-                    
-                    country.borders.map((border,index) => {
-                        
-                    return (
-                      <button key={index} className={styles.btn}>{border}</button>
-                     )
-                     })
-                     
-                }
-                </div>
-                </div>
-            </main>
+   
+    return (
+        <div>
+        <Header />
+        {
+            currentCountry && <DetailPage  country={currentCountry} />
+        }
             
             
-
-        </>
+        </div>
     )
+ 
 
 }
